6d56e87f34c297cf43a06a3681e9d6fdfa7f70b6
TEERAWATL <teerawatman0@gmail.com>
2022-02-06 14:55:23 +0700

    Create Exercise4_Teerawat_L.py

---
+++ Exercise4_Teerawat_L.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+score_FE = 50.0
+score_GB = 80.1
+score_ICS = 90.5
+score_CP = 89.5
+
+print("---Your Score---")
+
+print("Foundation English :", score_FE)
+
+print("General Business :", score_GB)
+
+print("Introduction to Computer Systems :", score_ICS)
+
+print("Computer Programming :", score_CP) 
+
+
+# In[ ]:
+
+
+
+
