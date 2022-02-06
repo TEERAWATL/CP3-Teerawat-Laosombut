8669b84e7d6b3f5198ff5eee8787c396a0a1bab1
TEERAWATL <teerawatman0@gmail.com>
2022-02-06 17:08:06 +0700

    Create Exercise5_2_Teerawat_L.py

---
+++ Exercise5_2_Teerawat_L.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s = int(input("Distance (km):"))
+t = int(input("Movement time (minute):"))
+t = t/60
+v = s/t
+print("Speed=", v, "Km/hr")
+
+
+# In[ ]:
+
+
+
+
