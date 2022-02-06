2701fae4d6d74d8c91c9e7540c9db032a3b48dd0
TEERAWATL <teerawatman0@gmail.com>
2022-02-06 17:04:07 +0700

    Create Exercise5_1_Teerawat_L.py

---
+++ Exercise5_1_Teerawat_L.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 2
+b = 1
+print("2 + 1 =", a+b)
+print("2 - 1 =", a-b)
+print("2 * 1 =", a*b)
+print("2 / 1 =", a/b)
+
+
+# In[ ]:
+
+
+
+
