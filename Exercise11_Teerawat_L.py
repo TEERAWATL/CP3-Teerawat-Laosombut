44511d72cac41690c6dc73b66da698f6c4f91513
TEERAWATL <teerawatman0@gmail.com>
2022-02-08 18:16:37 +0700

    Create Exercise11_Teerawat_L.py

---
+++ Exercise11_Teerawat_L.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+number = int(input("Enter the number:"))
+
+for i in range(number):
+    print(" "*(number-i),"*"*(i+1)+"*"*i)
+
+
+# In[ ]:
+
+
+
+
